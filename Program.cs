76e7a32b1381e0b9ab96c2b0043e375969ded32c
Zepp <zeppsoftapp@gmail.com>
2024-07-31 17:49:50 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.SqlClient;
+
+namespace RiderSQL;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string connectionString =
+            "Server=localhost,1433\\Catalog=Test;Database=Test;User=SA;Password=Zaratustra2020;";
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            try
+            {
+                HasRows(con);   
+            }
+            catch (Exception ex) 
+            { 
+                con.Close();
+            }
+        }
+    }
+    static void HasRows(SqlConnection connection)
+    {
+        using (connection)
+        {
+            SqlCommand command = new(
+                "SELECT * FROM products;",
+                connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var price = reader.GetSqlValue(0);
+                    var productName = reader.GetSqlValue(1);
+
+                    Console.WriteLine($"Price = {price?.ToString()}, Product_name = {productName?.ToString()}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+        }
+    }
+}
